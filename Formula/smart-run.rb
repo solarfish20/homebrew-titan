cb13d8a7f26b89b1252e65aa8643386932420cda
Kerry Blake Whiteside <kerryblakewhiteside@Kerrys-Mac-mini.local>
2025-10-04 22:02:00 -0600

    update sha256 for v1.0.0 (c18f16ebf2bc1ddb3fed7c7b0da02abaae26da131e802f24fcea56037c5eeea7)

--- Formula/smart-run.rb
+++ Formula/smart-run.rb
@@ -2,7 +2,7 @@ class SmartRun < Formula
   desc "Auto-detects script type and runs it in the right environment (teaches as it runs)"
   homepage "https://github.com/solarfish20/smart-run"
   url "https://github.com/solarfish20/smart-run/releases/download/v1.0.0/smart-run-1.0.0.tar.gz"
-  sha256 "75604fbfe5d4e60c4ebf104ae3d355af73990897ad0a2a50040648a39a18b067"
+  sha256 "c18f16ebf2bc1ddb3fed7c7b0da02abaae26da131e802f24fcea56037c5eeea7"
   license "MIT"
   def install
     bin.install "smart-run"
